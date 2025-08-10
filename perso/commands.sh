f9d18e7ad37aa98c4a778136b5e3b4e0ba07c26f
Artem Miniailo <artem.miniailo@gmail.com>
2025-08-10 17:21:49 +0200

    updates

--- perso/commands.sh
+++ perso/commands.sh
@@ -1,6 +1,20 @@
 sudo systemctl restart comfyui.service
 
-compute ssh comfyvm --zone europe-west1-c
+cd ~/.ssh
+gcloud compute os-login ssh-keys add --key-file=comfy_vm_key.pub
 
-gcloud compute scp --recurse comfyvm:~/ComfyUI/output /Users/artem/Downloads/output-local --zone=europe-west1-c
+gcloud compute ssh comfyvm --zone europe-west3-b
+
+gcloud compute scp --recurse comfyvm:~/ComfyUI/output /Users/artem/Downloads/output-local --zone=europe-west3-b
 scp -i ~/.ssh/comfy_vm_key artem@34.78.28.204:~/output.zip ~/Downloads/
+
+#***
+git clone https://github.com/cosaques/comfyui-on-cloud
+chmod +x ./comfyui-on-cloud/src/install.sh
+chmod +x ./comfyui-on-cloud/src/virgin_vm.sh
+chmod +x ./comfyui-on-cloud/perso/models-install.sh
+
+./comfyui-on-cloud/src/virgin_vm.sh # run this only for new VM. This will install miniconda, cuda 11.8, torch.
+source ~/.bashrc
+./comfyui-on-cloud/src/install.sh
+./comfyui-on-cloud/perso/models-install.sh
